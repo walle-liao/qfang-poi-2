66788f002c087cfda16192c36257426fb584e533
walle <liaozhicheng.cn@163.com>
2017-07-07 00:00:10 +0800

    modify

--- src/main/java/com/qfang/poi/excel/export/data/RedisCacheDataCollector.java
+++ src/main/java/com/qfang/poi/excel/export/data/RedisCacheDataCollector.java
@@ -1,9 +1,11 @@
 package com.qfang.poi.excel.export.data;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
+import org.apache.commons.collections.CollectionUtils;
 import org.springframework.data.redis.core.BoundListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * TODO
@@ -14,25 +16,56 @@ import org.springframework.data.redis.core.RedisTemplate;
  */
 public class RedisCacheDataCollector<T> implements DataCollector<T> {
 
+	private final static int DEFAULT_FETCH_STEP = 500;
+
 	private final String key;
-	private final AtomicInteger liveTaskSize;
 	private final BoundListOperations<String, T> listOperations;
+	private final int totalSize;
+	private final int fetchStep;
+	private final LinkedList<T> fetchCachedList;
+	private int readIndex = 0;
+	private int fetchIndex = 0;
 	
-	public RedisCacheDataCollector(String key, AtomicInteger liveTaskSize, RedisTemplate<String, T> redisTemplate) {
+	public RedisCacheDataCollector(String key, RedisTemplate<String, T> redisTemplate) {
+		this(key, redisTemplate, DEFAULT_FETCH_STEP);
+	}
+
+	public RedisCacheDataCollector(String key, RedisTemplate<String, T> redisTemplate, int fetchStep) {
 		this.key = key;
-		this.liveTaskSize = liveTaskSize;
 		this.listOperations = redisTemplate.boundListOps(key);
+		this.totalSize = Math.toIntExact(this.listOperations.size());
+		this.fetchStep = fetchStep;
+		this.fetchCachedList = new LinkedList<>();
 	}
 	
 	@Override
 	public boolean hasNext() {
-//		return this.liveTaskSize.get() > 0 || listOperations.
-		return true;
+		return readIndex < totalSize;
 	}
 
 	@Override
 	public T next() {
-		return null;
+		if(!fetchCachedList.isEmpty()) {
+			readIndex++;
+			return fetchCachedList.removeFirst();
+		}
+
+		this.doFetch();
+		readIndex++;
+		return fetchCachedList.removeFirst();
+	}
+
+	private void doFetch() {
+		List<T> tmpList = listOperations.range(fetchIndex, fetchIndex + this.fetchStep);
+		if(CollectionUtils.isEmpty(tmpList)) {
+			// 如果抓取不到数据，则让 excel 写线程立即跳出循环
+			// 这里读不到数据，可能会有以下原因导致：
+			// 1、此时缓存中对应的 key 刚好失效
+			// 2、缓存服务器不可用
+			this.readIndex = this.totalSize;
+		}
+		fetchIndex += this.fetchStep;
+		fetchCachedList.addAll(tmpList);
 	}
 
 }
